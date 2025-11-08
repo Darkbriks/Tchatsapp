b78e61ea9af1d31e209e72aa63f057867e6d7f6a
antoine <antoine.patron@etu.univ-littoral.fr>
2025-11-08 13:22:17 +0100

    + None message type * Implement conversion methods

--- src/main/java/fr/uga/im2ag/m1info/chatservice/common/MessageType.java
+++ src/main/java/fr/uga/im2ag/m1info/chatservice/common/MessageType.java
@@ -1,5 +1,6 @@
 package fr.uga.im2ag.m1info.chatservice.common;
 
+/** Enum representing different types of messages. */
 public enum MessageType {
     TEXT,
     MEDIA,
@@ -15,8 +16,63 @@ public enum MessageType {
     MESSAGE_REACTION,
     NOTIFICATION,
     EMPTY,
-    ERROR;
+    ERROR,
+    NONE;
 
-    public static MessageType fromByte(byte b){return MessageType.values()[b];}
-    public byte  toByte(){return 0;}
+    /** Convert an integer to a MessageType enum.
+     *
+     * @param i the integer to convert
+     * @return the corresponding MessageType, or NONE if no match is found
+     */
+    public static MessageType fromInt(int i) {
+        for (MessageType type : MessageType.values()) {
+            if (type.ordinal() == i) {
+                return type;
+            }
+        }
+        return NONE;
+    }
+
+    /** Convert a byte to a MessageType enum.
+     *
+     * @param b the byte to convert
+     * @return the corresponding MessageType, or NONE if no match is found
+     */
+    public static MessageType fromByte(byte b) {
+        return fromInt(b);
+    }
+
+    /** Convert this MessageType enum to a byte.
+     *
+     * @return the byte representation of this MessageType
+     */
+    public byte toByte() {
+        return (byte) this.ordinal();
+    }
+
+    /** Convert this MessageType enum to a string.
+     *
+     * @return the string representation of this MessageType
+     */
+    @Override
+    public String toString() {
+        return switch (this) {
+            case TEXT -> "TEXT";
+            case MEDIA -> "MEDIA";
+            case CREATE_USER -> "CREATE_USER";
+            case CREATE_GROUP -> "CREATE_GROUP";
+            case UPDATE_PSEUDO -> "UPDATE_PSEUDO";
+            case ADD_CONTACT -> "ADD_CONTACT";
+            case ADD_GROUP_MEMBER -> "ADD_GROUP_MEMBER";
+            case REMOVE_GROUP_MEMBER -> "REMOVE_GROUP_MEMBER";
+            case UPDATE_GROUP_NAME -> "UPDATE_GROUP_NAME";
+            case DELETE_GROUP -> "DELETE_GROUP";
+            case LEAVE_GROUP -> "LEAVE_GROUP";
+            case MESSAGE_REACTION -> "MESSAGE_REACTION";
+            case NOTIFICATION -> "NOTIFICATION";
+            case EMPTY -> "EMPTY";
+            case ERROR -> "ERROR";
+            case NONE -> "NONE";
+        };
+    }
 }
